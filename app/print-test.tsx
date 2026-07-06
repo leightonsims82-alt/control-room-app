035b40f0fdaa82334ed2a07343e4a66bdc93a08a
leightonsims82-alt <leightonsims82@gmail.com>
2026-07-06 14:47:03 +0100

    Add print test route

---
+++ app/print-test.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function PrintTest() {
+  return (
+    <View style={{ padding: 24 }}>
+      <Text>Print Test</Text>
+    </View>
+  );
+}
