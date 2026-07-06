57138ed3b9cf4870379fff4e5d310b85b7f2c887
leightonsims82-alt <leightonsims82@gmail.com>
2026-07-06 21:34:03 +0100

    Keep both gables rows in stored templates

--- data/sitePlannerStore.tsx
+++ data/sitePlannerStore.tsx
@@ -8,10 +8,10 @@ const SITE_DELAYS_KEY = 'siteprog:week-based-delays:v2';
 const TRADE_CONTACTS_KEY = 'siteprog:trade-contacts:v1';
 const ISSUE_SETTINGS_KEY = 'siteprog:issue-settings:v1';
 const ISSUE_LOGS_KEY = 'siteprog:issue-logs:v1';
-const PLOT_TEMPLATES_KEY = 'siteprog:plot-templates:v3';
+const PLOT_TEMPLATES_KEY = 'siteprog:plot-templates:v5';
 const SITE_PROGRAMME_SETUP_KEY = 'siteprog:programme-setup:v1';
 
-const REMOVED_TEMPLATE_ACTIVITY_CODES = new Set(['BOARD', '5TH SCAFF', 'SS', 'GABLES 1', 'GABLES 2']);
+const REMOVED_TEMPLATE_ACTIVITY_CODES = new Set(['BOARD', '5TH SCAFF', 'SS']);
 const RENAMED_TEMPLATE_ACTIVITY_CODES: Record<string, string> = {
   FND: 'Foundation',
   DNG: 'Drainage',
@@ -24,6 +24,7 @@ const RENAMED_TEMPLATE_ACTIVITY_CODES: Record<string, string> = {
   '1ST ELEC': '1ST FIX ELEC',
   '1ST SPRINKLER': '1ST FIX SPRINKLER',
   PP: 'PRE-PLASTER QA',
+  GABLES: 'GABLES 2',
 };
 
 export type TradeContact = { id: string; trade: string; contractor: string; supervisorName: string; supervisorEmail: string; supervisorPhone: string };
